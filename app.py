c2ee3d086732309a7860fff1bd943aa5f6a032f3
akcsri <akihiko.kaneko@csri-japan.com>
2026-07-16 12:27:25 +0900

    Modify message event response in app.py
    Updated message handling to respond in Japanese.

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
+import os
+
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+app = App(
+    token=os.environ["SLACK_BOT_TOKEN"]
+)
+
 @app.event("message")
 def handle_message(event, say):
-
-    print("EVENT RECEIVED")
-    print(event)
 
     if event.get("subtype"):
         return
 
-    say("イベント受信")
+    say("受信しました")
+
+
+if __name__ == "__main__":
+
+    print("BOT START")
+
+    handler = SocketModeHandler(
+        app,
+        os.environ["SLACK_APP_TOKEN"]
+    )
+
+    handler.start()
